e63009bdd6ced4e3997dba3d6b994fe63c571e66
Vasu Udompetaikul <135559205+vasu-kmitl@users.noreply.github.com>
2026-02-08 11:04:36 +0700

    Create tsae_robot_step1_v1.m
    Initial version

---
+++ tsae_robot_step1_v1.m
@@ -0,0 +1,56 @@
+% -----------------------------------
+% TSAE Robot Competation: Step 1
+% AE KMITL
+% -----------------------------------
+clc; clear; close all; format compact
+
+% Basic Geometry
+w  = 10;  h  = 10;
+a  =  5;  b  =  5;  c = 5; 
+W  = 50;  H  = 50;
+Nx =  5;  Ny =  5;
+px = W/Nx; py = H/Ny;
+
+% Sketch base, table and frame 
+base = [-w/2 -h/2;
+         w/2 -h/2;
+         w/2  h/2;
+        -w/2  h/2;
+        -w/2 -h/2];
+
+table = [0 -H/2;
+         W -H/2;
+         W  H/2;
+         0  H/2;
+         0 -H/2] + [w/2+a+b 0];
+
+frame = [ -b -H/2-c;
+         W+b -H/2-c;
+         W+b  H/2+c;
+          -b  H/2+c;
+          -b -H/2-c] + [w/2+a+b 0];
+
+figure; hold on; grid on; axis equal; 
+plot(base(:,1),base(:,2))
+plot(table(:,1),table(:,2))
+plot(frame(:,1),frame(:,2))
+
+for j = 1:Ny
+    for i = 1:Nx
+        xPos = i*px + a + b;
+        yPos = (2*j - Ny - 1)*py/2;
+        plot(xPos, yPos, 'ro'); 
+    end
+end
+
+X = input('X: ');
+Y = input('Y: ');
+x = X*px + a + b;
+y = (2*Y - Ny - 1)*py/2;
+R = sqrt(x^2 + y^2)
+phi1 = atan2d(y,x);
+theta1 = phi1+90
+
+arm = [0 0;
+       x y];
+plot(arm(:,1),arm(:,2))
